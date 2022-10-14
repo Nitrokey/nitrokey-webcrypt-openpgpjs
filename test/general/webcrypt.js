4f4a2a832ef4ed650b56569a7d45766a6f62f9a2
Szczepan Zalega <szczepan@nitrokey.com>
2022-10-14 13:44:59 +0200

    Fixups to test skipping

--- test/general/webcrypt.js
+++ test/general/webcrypt.js
@@ -38,8 +38,9 @@ module.exports = () => describe('OpenPGP.js webcrypt public api tests', function
   describe('WebCrypt general - unit tests', function () {
 
     before(function() {
-      const webauthn_available = typeof window !== undefined && window.PublicKeyCredential !== undefined;
+      const webauthn_available = typeof window !== 'undefined' && window.PublicKeyCredential !== undefined;
       if (!webauthn_available) {
+        // eslint-disable-next-line no-invalid-this
         this.skip();
       }
     });
